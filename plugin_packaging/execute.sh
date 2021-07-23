5074ea0229c7a93e30eb07f5a9a65708bfd4b8eb
atrautsch <atrautsch@users.noreply.github.com>
2021-07-23 14:08:32 +0200

    project-name not used

--- plugin_packaging/execute.sh
+++ plugin_packaging/execute.sh
@@ -5,7 +5,7 @@ NEW_UUID=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
 
 cp -R $REPOSITORY_PATH "/dev/shm/$NEW_UUID"
 
-COMMAND="java -jar $PLUGIN_PATH/target/refSHARK.jar --input /dev/shm/$NEW_UUID --rev $3 --url $4 --project_name ${5} --db-hostname $6 --db-port $7 --db-database $8"
+COMMAND="java -jar $PLUGIN_PATH/target/refSHARK.jar --input /dev/shm/$NEW_UUID --rev $3 --url $4 --db-hostname $6 --db-port $7 --db-database $8"
 
 if [ ! -z ${13+x} ] && [ ${13} != "None" ]; then
     COMMAND="$COMMAND --debug ${13}"
